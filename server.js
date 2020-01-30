b5190e39cee0088f39417018ac7a58cf3e854ee5
Allison Wagner <allisonjw01@gmail.com>
2020-01-29 21:00:02 -0700

    Gets post to work for bands

--- server.js
+++ server.js
@@ -14,22 +14,11 @@ app.get('/', (request, response) => {
   response.send('View Boy Bands and the Members');
 });
 
+// get, get by id and post bands
 app.get('/api/v1/bands', (request, response) => {
     database('bands').select()
     .then((bands) => {
       response.status(200).json(bands);
-    })
-    .catch((error) => {
-      response.status(500).json({
-        error: 'Sorry there were was a problem connecting to the database.'
-      });
-    })
-});
-
-app.get('/api/v1/members', (request, response) => {
-    database('members').select()
-    .then((members) => {
-      response.status(200).json(members);
     })
     .catch((error) => {
       response.status(500).json({
@@ -53,6 +42,40 @@ app.get('/api/v1/bands/:id', (request, response) => {
     .catch((error) => {
       response.status(500).json({
         error: 'Sorry there were was a problem connecting to the database.'
+    });
+  })
+});
+
+app.post('/api/v1/bands', async (request, response) => {
+  const newBand = request.body;
+
+  for (let requiredParameter of ['band', 'highest_song', 'featuring_artist', 'highest_song_vid']) {
+    if (!newBand[requiredParameter]) {
+      return response
+        .status(422)
+        .json({ error: `Expected format: { band: <String>, highest_song: <String> , featuring_artist: <String>, highest_song_vid: <String>}. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('bands').insert(newBand, 'id')
+    .then(band => {
+      response.status(201).json(`Boy band with id of ${band[0]} successfully created!`)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+  })
+});
+
+// get, get by id and post members
+
+app.get('/api/v1/members', (request, response) => {
+  database('members').select()
+  .then((members) => {
+    response.status(200).json(members);
+  })
+  .catch((error) => {
+    response.status(500).json({
+      error: 'Sorry there were was a problem connecting to the database.'
     });
   })
 });
