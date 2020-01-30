6136cde2e7e199b0abc32fab6a999bbd7568e830
Allison Wagner <allisonjw01@gmail.com>
2020-01-29 17:58:31 -0700

    Gets 3 endpoints fetching data

--- server.js
+++ server.js
@@ -1,50 +1,57 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(express.json());
 app.use(cors());
-
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'BYOB';
 
 app.get('/', (request, response) => {
-  response.send('Oh hey BYOB');
+  response.send('View Boy Bands and the Members');
 });
 
 app.get('/api/v1/bands', (request, response) => {
     database('bands').select()
     .then((bands) => {
-      if (!bands) {
-        return response.status(404).send({
-          error: 'The bands data can not be found.'
-        });
-      }
       response.status(200).json(bands);
     })
     .catch((error) => {
-      response.status(500).send({
+      response.status(500).json({
         error: 'There were problems connecting to the database.'
-    });
-  })
+      });
+    })
+});
+
+app.get('/api/v1/members', (request, response) => {
+    database('members').select()
+    .then((members) => {
+      response.status(200).json(members);
+    })
+    .catch((error) => {
+      response.status(500).json({
+        error: 'There were problems connecting to the database.'
+      });
+    })
 });
 
 app.get('/api/v1/bands/:id', (request, response) => {
   const { id } = request.params;
-  database('bands').select()
-    .then((bands) => {
-      const selectedBand = bands.find((set) => {
-        return set.inc_id === parseInt(id)
-      })
-      if (!selectedBand) {
-        return response.status(404).send({
-          error: 'The set data you are looking for can not be found. Please try another set id.'
-        });
+  database('bands')
+    .where({ id: id })
+    .then(band => {
+      if (band.length === 0) {
+        response
+          .status(404)
+          .json({ error: `There is not a boy band with an id of ${id}!` });
       }
-      response.status(200).json(selectedSet);
+      response.status(200).json(band[0]);
     })
     .catch((error) => {
-      response.status(500).send({
+      response.status(500).json({
         error: 'There were problems connecting to the database.'
     });
   })
